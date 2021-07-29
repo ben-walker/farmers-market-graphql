a0265b527230c0fb69a2492f2893378454f4feec
Ben Walker <bwalke05@gmail.com>
2021-07-29 19:21:56 -0400

    Add pre-run generation back to dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,4 +5,5 @@ export DATABASE_URL="postgresql://postgres:password@localhost:5432"
 # Install node_modules if missing
 [ ! -d "node_modules" ] && npm ci
 
-npx concurrently --kill-others "docker compose up" "npm:dev:*"
+npm run generate \
+&& npx concurrently --kill-others "docker compose up" "npm:dev:*"
