23ffbe5297a07cac5ed743cd86c6ed209d9f3741
Ben Walker <bwalke05@gmail.com>
2021-07-28 20:33:15 -0400

    Improve dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,6 +2,9 @@
 
 export DATABASE_URL="postgresql://postgres:password@localhost:5432"
 
+# Install node_modules if missing
+[ ! -d "node_modules" ] && npm ci
+
 npm run generate \
 && docker compose up --detach \
-&& npm run watch:src
+&& npx concurrently --kill-others npm:watch:*
