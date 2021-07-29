f24eb38a4b8b550ab47f8fb94955f8fb37cb5691
Ben Walker <bwalke05@gmail.com>
2021-07-29 15:49:03 -0400

    Simplify dev script setup

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,5 +5,4 @@ export DATABASE_URL="postgresql://postgres:password@localhost:5432"
 # Install node_modules if missing
 [ ! -d "node_modules" ] && npm ci
 
-npm run generate \
-&& npx concurrently --kill-others "docker compose up" "npm:watch:*"
+npx concurrently --kill-others "docker compose up" "npm:dev:*"
