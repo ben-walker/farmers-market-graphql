38302b16ac1ae889a9d95d2bf9b32889940a5949
Ben Walker <bwalke05@gmail.com>
2021-07-30 19:35:49 -0400

    Add passwordHash to schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,10 @@ const prisma = new PrismaClient();
 
 export const seed = async (): Promise<void> => {
   await prisma.user.createMany({
-    data: [{ email: "fox@email.com" }, { email: "cow@email.com" }],
+    data: [
+      { email: "fox@email.com", passwordHash: "" },
+      { email: "cow@email.com", passwordHash: "" },
+    ],
     skipDuplicates: true,
   });
 };
