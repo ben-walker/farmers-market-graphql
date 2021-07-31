d3e7a5401c290371163980ac867761dd3427ce96
Ben Walker <bwalke05@gmail.com>
2021-07-30 20:32:30 -0400

    Add legit passwordHash to seeded users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { argon2id, hash } from "argon2";
 
 const prisma = new PrismaClient();
 
 export const seed = async (): Promise<void> => {
+  const passwordHash = await hash("password", { type: argon2id });
+
   await prisma.user.createMany({
     data: [
-      { email: "fox@email.com", passwordHash: "" },
-      { email: "cow@email.com", passwordHash: "" },
+      { email: "fox@email.com", passwordHash },
+      { email: "cow@email.com", passwordHash },
+      { email: "dog@email.com", passwordHash },
     ],
     skipDuplicates: true,
   });
