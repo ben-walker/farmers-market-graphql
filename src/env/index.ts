f3fb07641709348a1541bfd6cf2b41b206f77620
Ben Walker <bwalke05@gmail.com>
2021-07-31 10:35:53 -0400

    Setup express session

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,3 +2,4 @@ const { env } = process;
 
 export const PORT = env.PORT || 4000;
 export const NODE_ENV = env.NODE_ENV || "development";
+export const SECRET = env.SECRET || "oo6iP6dQRC/qm2WlUn3Tej+LocObV801";
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import session, { SessionOptions } from "express-session";
 
 import { context } from "./context";
-import { PORT } from "./env";
+import { PORT, SECRET } from "./env";
 import { schema } from "./schema";
+
+const sessionOptions: SessionOptions = {
+  cookie: {
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1_000, // 30 days
+    sameSite: true,
+    secure: true,
+  },
+  name: "user_session",
+  saveUninitialized: false,
+  rolling: true,
+  secret: SECRET,
+};
 
 const boot = async () => {
   const server = new ApolloServer({
@@ -13,6 +27,7 @@ const boot = async () => {
   await server.start();
 
   const app = express();
+  app.use(session(sessionOptions));
   server.applyMiddleware({ app });
 
   app.listen(PORT, () => {
