371950f8f769ae271a68a285dff536cd230dd152
Ben Walker <bwalke05@gmail.com>
2021-08-01 18:05:58 -0400

    Fix type error

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,5 +2,5 @@ const { env } = process;
 
 export const PORT = env.PORT || 4000;
 export const NODE_ENV = env.NODE_ENV || "development";
-export const SECRET = env.SECRET;
+export const SECRET = env.SECRET || "";
 export const REDIS_URL = env.REDIS_URL;
