390f4ba6e40609978a104c9fa7dd9722093a2153
Ben Walker <bwalke05@gmail.com>
2021-08-10 21:53:43 -0400

    Add apollo studio to cors origin

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,6 +3,7 @@
 export DATABASE_URL="postgresql://postgres:password@localhost:5432"
 export REDIS_URL="redis://:@localhost:6379"
 export SECRET="oo6iP6dQRC/qm2WlUn3Tej+LocObV801"
+export CORS_ORIGIN="https://studio.apollographql.com"
 
 # Install node_modules if missing
 [ ! -d "node_modules" ] && npm ci
