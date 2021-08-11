0365918c804f6d06e9fbbb118feb893aaca33a4d
Ben Walker <bwalke05@gmail.com>
2021-08-10 21:47:18 -0400

    Add cors origin env

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,3 +4,4 @@ export const PORT = env.PORT || 4000;
 export const NODE_ENV = env.NODE_ENV || "development";
 export const SECRET = env.SECRET || "";
 export const REDIS_URL = env.REDIS_URL;
+export const CORS_ORIGIN = env.CORS_ORIGIN?.split(",") || true;
