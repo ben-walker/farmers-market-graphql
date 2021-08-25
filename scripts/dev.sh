4f0246e14fba4409e25661ff555be3ce3476b79a
Ben Walker <bwalke05@gmail.com>
2021-08-24 20:51:20 -0400

    Simplify dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-function wait_for_postgres {
-  while ! pg_isready --host=localhost --port=5432 --username=postgres; do
-    sleep 1
-  done
-}
-
 # Setup the environment
 export DATABASE_URL="postgresql://postgres:password@localhost:5432"
 export REDIS_URL="redis://:@localhost:6379"
@@ -15,7 +9,11 @@ export CORS_ORIGIN="https://studio.apollographql.com"
 # Install node_modules if missing
 [ ! -d "node_modules" ] && npm install
 
-docker compose up --detach \
-  && wait_for_postgres \
-  && npm run prisma:db:sync \
-  && npx concurrently --kill-others npm:*:dev
+docker compose up --detach
+
+while ! pg_isready --host=localhost --port=5432 --username=postgres; do
+  sleep 1
+done
+
+npm run prisma:db:sync
+npx concurrently --kill-others npm:*:dev
