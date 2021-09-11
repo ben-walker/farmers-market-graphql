a5126ab0c3ec0f04bdd454006b544f23701c6c26
Ben Walker <bwalke05@gmail.com>
2021-09-11 11:25:49 -0400

    Add prisma studio script

---
+++ scripts/studio.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Setup the environment
+export DATABASE_URL="postgresql://postgres:password@localhost:5432"
+
+# Install node_modules if missing
+[ ! -d "node_modules" ] && npm install
+
+docker compose up --detach
+
+while ! pg_isready --host=localhost --port=5432 --username=postgres; do
+  sleep 1
+done
+
+npx prisma migrate dev
+npx prisma db seed
+npx prisma studio
