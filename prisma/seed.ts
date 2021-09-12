02e8911f10caab2e8882441a659f6302a9eb352e
Ben Walker <bwalke05@gmail.com>
2021-09-12 17:35:17 -0400

    Add query to find nearby shops

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,11 +19,36 @@ const seed = async (): Promise<void> => {
 
   await prisma.shop.createMany({
     data: [
-      { id: "1", name: "Fox's Pasta Emporium", ownerId: "1" },
-      { id: "2", name: "Cow's Spicy Enchiladas", ownerId: "2" },
-      { id: "3", name: "Dog's Raw Nut Butters", ownerId: "3" },
-      { id: "4", name: "Cat's Elegant Coffee", ownerId: "4" },
-      { id: "5", name: "Bull's Decadent Legumes", ownerId: "5" },
+      {
+        id: "1",
+        name: "Fox's Pasta Emporium",
+        serveRadius: 3000,
+        ownerId: "1",
+      },
+      {
+        id: "2",
+        name: "Cow's Spicy Enchiladas",
+        serveRadius: 5000,
+        ownerId: "2",
+      },
+      {
+        id: "3",
+        name: "Dog's Raw Nut Butters",
+        serveRadius: 10000,
+        ownerId: "3",
+      },
+      {
+        id: "4",
+        name: "Cat's Elegant Coffee",
+        serveRadius: 1500,
+        ownerId: "4",
+      },
+      {
+        id: "5",
+        name: "Bull's Decadent Legumes",
+        serveRadius: 12000,
+        ownerId: "5",
+      },
     ],
     skipDuplicates: true,
   });
--- src/schema/resolvers/Shop.ts
+++ src/schema/resolvers/Shop.ts
@@ -4,10 +4,55 @@ import {
   FindManyShopResolver,
   FindUniqueShopResolver,
   GroupByShopResolver,
+  Shop,
   ShopRelationsResolver,
 } from "@generated/type-graphql";
+import {
+  Args,
+  ArgsType,
+  Ctx,
+  Field,
+  Float,
+  Query,
+  Resolver,
+} from "type-graphql";
+
+import { Context } from "../../context";
+
+@ArgsType()
+class SpatialSearchArgs {
+  @Field(() => Float)
+  latitude!: number;
+
+  @Field(() => Float)
+  longitude!: number;
+}
+
+@Resolver(() => Shop)
+export class ShopResolver {
+  @Query(() => [Shop])
+  async shopSpatialSearch(
+    @Args() { latitude, longitude }: SpatialSearchArgs,
+    @Ctx() { prisma }: Context
+  ): Promise<Shop[]> {
+    const shops = await prisma.$queryRaw<Shop[]>`
+      SELECT *
+      FROM "Shop" AS shop, "ShopCoordinates" AS coords
+      WHERE shop.id = coords."shopId"
+        AND earth_box(ll_to_earth(${latitude}, ${longitude}), shop."serveRadius") @> ll_to_earth(coords.latitude, coords.longitude)
+        AND earth_distance(ll_to_earth(${latitude}, ${longitude}), ll_to_earth(coords.latitude, coords.longitude)) <= shop."serveRadius"
+    `;
+
+    // Serialize values for typegraphql
+    return shops.map((shop) => ({
+      ...shop,
+      createdAt: new Date(shop.createdAt),
+    }));
+  }
+}
 
 export const shopResolvers = [
+  ShopResolver,
   AggregateShopResolver,
   FindFirstShopResolver,
   FindManyShopResolver,
